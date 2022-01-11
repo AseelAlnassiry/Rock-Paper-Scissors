1a6fb830fafa121d85140de67e22c595f7651f33
Aseel <aseelalnassiry@gmail.com>
2022-01-10 20:25:51 -0800

    Hello there Joel

--- script.js
+++ script.js
@@ -11,31 +11,31 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === choices[0]) {
     if (computerSelection === choices[0]) {
-      return "Tie! Both chose Rock";
+      return ["Tie! Both chose Rock", 0];
     } else if (computerSelection === choices[1]) {
-      return "You Lose! Paper beats Rock";
+      return ["You Lose! Paper beats Rock", -1];
     } else {
-      return "You Win! Rock beats Scissor";
+      return ["You Win! Rock beats Scissor", 1];
     }
   }
 
   if (playerSelection === choices[1]) {
     if (computerSelection === choices[0]) {
-      return "You Win! Rock beats Paper";
+      return ["You Win! Rock beats Paper", 1];
     } else if (computerSelection === choices[1]) {
-      return "Tie! Both chose Paper";
+      return ["Tie! Both chose Paper", 0];
     } else {
-      return "You Lose! Scissor beats Paper";
+      return ["You Lose! Scissor beats Paper", -1];
     }
   }
 
   if (playerSelection === choices[2]) {
     if (computerSelection === choices[0]) {
-      return "You Lose! Rock beats Scissor";
+      return ["You Lose! Rock beats Scissor", -1];
     } else if (computerSelection === choices[1]) {
-      return "You Win! Scissor beats Paper";
+      return ["You Win! Scissor beats Paper", 1];
     } else {
-      return "Tie! Both chose Scissor";
+      return ["Tie! Both chose Scissor", 0];
     }
   }
 }
@@ -46,10 +46,28 @@ function game() {
   console.log(outcome);
 }
 
-const btn = document.querySelectorAll("button");
+let btn = document.querySelectorAll("button");
+
+let round = document.querySelector("#counter");
+let roundCounter = round.textContent[round.textContent.length - 1];
+console.log(roundCounter);
+
+let announcementBoard = document.querySelector("#announcement");
+
+let internalCounter = 0;
 
 btn.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(playRound(button.textContent, computerPlay()));
+    if (roundCounter > 0) {
+      const currentRound = playRound(button.textContent, computerPlay());
+      announcementBoard.textContent = currentRound[0];
+      internalCounter = internalCounter + currentRound[1];
+      roundCounter--;
+      round.textContent =
+        round.textContent.slice(0, 8) + roundCounter.toString();
+    } else {
+      round.textContent = " Rounds: 5";
+      roundCounter = round.textContent[round.textContent.length - 1];
+    }
   });
 });
